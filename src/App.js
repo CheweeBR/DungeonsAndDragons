769f5f6834eb5b0828256830c24685f611b86b59
Tiago Eloy Possidonio <105402460+CheweeBR@users.noreply.github.com>
2024-05-06 02:54:57 +0000

    busca

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import { useRef } from "react";
 
 function App() {
   const [componenteAtivo, setComponenteAtivo] = useState("Buscar");
-  let botaoValue = componenteAtivo === "Buscar" ? "List" : "Search";
+  const [termoPesquisa, setTermoPesquisa] = useState("");
+  const [resultadosPesquisa, setResultadosPesquisa] = useState([]);
+  let botaoValue = componenteAtivo === "Buscar" ? "Listar" : "Buscar";
+
   const exibirListar = () => {
     if (componenteAtivo === "Buscar") {
       setComponenteAtivo("Listar");
-      botaoValue = "Buscar";
     } else {
       setComponenteAtivo("Buscar");
-      botaoValue = "Listar";
     }
   };
 
@@ -18,9 +20,38 @@ function App() {
     if (componenteAtivo === "Listar") {
       return <ListMagiasComponent />;
     } else if (componenteAtivo === "Buscar") {
-      return <SearchMagiaComponent />;
+      return (
+        <SearchMagiaComponent
+          termoPesquisa={termoPesquisa}
+          setTermoPesquisa={setTermoPesquisa}
+          handleSearch={handleSearch}
+          setResultadosPesquisa={setResultadosPesquisa}
+        />
+      );
     } else {
       return "";
+    }
+  };
+
+  const handleSearch = async (searchTerm) => {
+    try {
+      const apiUrl = "https://www.dnd5eapi.co/api/spells/?name=" + searchTerm;
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error("Erro de conexão.");
+      }
+      const data = await response.json();
+      const filteredSpells = data.results.filter((spell) => {
+        return startsWithCaseInsensitive(spell.name, searchTerm);
+      });
+      console.log("Chegou aqui");
+      console.log(filteredSpells);
+      setResultadosPesquisa(filteredSpells);
+      return filteredSpells;
+    } catch (error) {
+      console.error("Error:", error);
+      setResultadosPesquisa([]);
+      return [];
     }
   };
 
@@ -38,11 +69,96 @@ function App() {
   );
 }
 
-function SearchMagiaComponent() {
+function SearchMagiaComponent({ handleSearch, setResultadosPesquisa }) {
+  const [termoPesquisa, setTermoPesquisa] = useState("");
+  const [sugestoesMagias, setSugestoesMagias] = useState([]);
+  const [magiaSelecionada, setMagiaSelecionada] = useState(null);
+  const timeoutRef = useRef(null);
+
+  const handleInputChange = (event) => {
+    const searchTerm = event.target.value;
+    setTermoPesquisa(searchTerm);
+
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      if (searchTerm.trim() !== "") {
+        handleSearch(searchTerm).then((sugestoes) => {
+          if (sugestoes && sugestoes.length > 0) {
+            setSugestoesMagias(sugestoes);
+          } else {
+            setSugestoesMagias([]);
+          }
+        });
+      } else {
+        setSugestoesMagias([]);
+      }
+    }, 300);
+  };
+
+  const handleSpellSelection = (magia) => {
+    setMagiaSelecionada(magia);
+  };
+
   return (
     <div id="CampoPesquisa">
       <p id="textSearch">Spell:</p>
-      <input type="text" name="search" id="search"></input>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        value={termoPesquisa}
+        onChange={handleInputChange}
+      />
+      {}
+      <></>
+      <ul id="listSearch">
+        {sugestoesMagias.map((magia, index) => (
+          <li key={index} id="searchAparence">
+            <button
+              id="searchAparence"
+              onClick={() => handleSpellSelection(magia)}
+            >
+              {magia.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+      {magiaSelecionada && (
+        <div>
+          <p>Magia selecionada: {magiaSelecionada.name}</p>
+          <div id="spellNamBox">
+            <h3 id="spellName">${magiaSelecionada.name}</h3>
+          </div>
+          <div id="othersInformation">
+            <p>
+              <strong>Escola:</strong> ${magiaSelecionada.school.name}
+            </p>
+            <p>
+              <strong>Nível:</strong> ${magiaSelecionada.level}
+            </p>
+            <p>
+              <strong>Tempo de Conjuração:</strong> $
+              {magiaSelecionada.casting_time}
+            </p>
+            <p>
+              <strong>Alcance:</strong> ${magiaSelecionada.range}
+            </p>
+            <p>
+              <strong>Componentes:</strong> $
+              {magiaSelecionada.components.join(", ")}
+            </p>
+            <p>
+              <strong>Duração:</strong> ${magiaSelecionada.duration}
+            </p>
+            <p>
+              <strong>Descrição:</strong> ${magiaSelecionada.desc.join("<br>")}
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
@@ -83,6 +199,10 @@ function listarMagias() {
       return result.results;
     })
     .catch((error) => console.error(error));
+}
+
+function startsWithCaseInsensitive(fullString, searchString) {
+  return fullString.toLowerCase().startsWith(searchString.toLowerCase());
 }
 
 export default App;
